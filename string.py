f10b4a4307051d831ea5067cd488353b983e4490
Laxmirawat0720 <155939431+Laxmirawat0720@users.noreply.github.com>
2024-12-01 15:49:20 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,19 @@
+my_string = "Hello, World!"
+
+uppercase_string = my_string.upper()
+print("Uppercase:", uppercase_string)
+
+lowercase_string = my_string.lower()
+print("Lowercase:", lowercase_string)
+
+starts_with_hello = my_string.startswith("Hello")
+print("Starts with 'Hello':", starts_with_hello)
+
+ends_with_world = my_string.endswith("World!")
+print("Ends with 'World!':", ends_with_world)
+
+replaced_string = my_string.replace("World", "Python")
+print("Replaced 'World' with 'Python':", replaced_string)
+
+words_list = my_string.split()
+print("List of words:", words_list)
